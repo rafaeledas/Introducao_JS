0d30afac91cec21411745d126b361220fd33208f
Rafael Eugenio de Araujo Souza <rafaeledas@outlook.com>
2020-02-25 11:53:17 -0300

    comandos no index.html e main.js

---
+++ main.js
@@ -0,0 +1,25 @@
+//var nome = "Rafael Souza";
+//var id = 35;
+//var id2 = 10;
+//var frase="Japão é o melhor time do mundo";
+//alert("Bem vindo ao treinamento de JavaScript "+nome+" tem " +id+" anos");
+//alert(id + id2)
+//console.log(nome);
+//console.log(id+id2);
+//console.log(frase.replace("Japão","Brasil"));
+//console.log(frase.toUpperCase());
+//console.log(frase.toLowerCase());
+//alert(frase.replace("Japão","Brasil"))
+//var lista =["maca","pera","laranja"]
+//console.log(lista[3]);
+//alert(lista[1])
+//lista.push("uva");
+//alert(lista[3]);
+//console.log(lista[4]);
+//lista.pop(lista[4]);
+//alert(lista[2]);
+//console.log(lista.length;
+//console.log(lista.toString());
+//console.log(lista.join(" | "));
+var fruta ={nome:"maça", cor:"vermelha"};
+console.log(fruta);
